1b6f91b136f3904d0884e7c1bf044fca17244373
rmil <rhys.milling@ystv.co.uk>
2022-03-20 18:26:23 +0000

    replace schema-data meta with correct script

--- schema-data/_meta.sql
+++ schema-data/_meta.sql
@@ -1,29 +1,14 @@
-\echo 'ystv_website2020 database-server initialisation'
-
-\if :{?env}
-\else
-    \set env 'dev'
-\endif
-\echo setting up a :"env" environment
-
-\echo 'creating database users...'
-\set admin_user 'ystv_website2020_':env'_admin'
-\set wapi_user 'ystv_website2020_':env'_wapi'
-CREATE USER :admin_user;
-CREATE USER :wapi_user;
-\echo 'created database users'
-
-\echo 'creating database...'
-\set db_name ystv_website2020_:env
-CREATE DATABASE :db_name WITH OWNER :admin_user;
-\echo 'created database'
-
-\echo 'removing default schema...'
-DROP SCHEMA IF EXISTS default;
-\echo 'removed default schema'
-
-\echo 'setting permissions...'
-REVOKE ALL ON DATABASE :db_name FROM PUBLIC;
-GRANT CREATE ON DATABASE :db_name TO :admin_user;
-GRANT SELECT, INSERT, DELETE, EXECUTE ON DATABASE :db_name TO :wapi_user;
-\echo 'permissions set'
+\echo 'ystv_website2020 database post-migration initialisation'
+\echo 'loading event...'
+\i event.sql
+\echo 'loaded event'
+\echo 'loading keycard...'
+\i keycard.sql
+\echo 'loaded keycard'
+\echo 'loading officership...'
+\i officership.sql
+\echo 'loaded officership'
+\echo 'loading viewcount...'
+\i viewcount.sql
+\echo 'loaded viewcount'
+\echo 'post-migration initialisation finished'
