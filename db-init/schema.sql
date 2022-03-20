78f7ef3269482195eac0d819ef20240c20586ed1
rmil <rhys.milling@ystv.co.uk>
2022-03-20 18:26:50 +0000

    add schema and privileges init

---
+++ db-init/schema.sql
@@ -0,0 +1,20 @@
+-- should be connecting to the website2020 db as the owner user  to run this
+-- inputs:
+-- - apps_role
+
+\echo 'creating schemas'
+CREATE SCHEMA people;
+CREATE SCHEMA video;
+CREATE SCHEMA event;
+CREATE SCHEMA misc;
+CREATE SCHEMA mail;
+CREATE SCHEMA creator;
+CREATE SCHEMA playout;
+
+GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA people TO :apps_role;
+GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA video TO :apps_role;
+GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA event TO :apps_role;
+GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA misc TO :apps_role;
+GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA mail TO :apps_role;
+GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA creator TO :apps_role;
+GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA playout TO :apps_role;
