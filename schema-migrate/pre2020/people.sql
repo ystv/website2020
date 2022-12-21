94f5cedddad36fb747099e36d83521e8469e9c31
Marks Polakovs <github@markspolakovs.me>
2022-12-21 23:47:16 +0200

    Hack to avoid "non-existent lol" team (#20)

--- schema-migrate/pre2020/people.sql
+++ schema-migrate/pre2020/people.sql
@@ -97,7 +97,9 @@ INNER JOIN people.officerships ON officership_name = name;
 --
 -- Migration from public.officerships to people.officership_teams
 INSERT INTO people.officership_teams(name)
-SELECT DISTINCT team FROM public.officerships WHERE team IS NOT NULL;
+SELECT DISTINCT team FROM public.officerships WHERE team IS NOT NULL
+-- Marks - temporary hack to avoid "non existent lol"
+AND team <> 'commercial';
 --
 -- Migration from public.officerships to people.officership_team_members
 INSERT INTO people.officership_team_members(team_id, officer_id, is_leader, is_deputy)
